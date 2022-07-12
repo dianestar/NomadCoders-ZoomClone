a59f95a3dbd1e2df434f6b412d074cce6d97832c
dianestar <dianestar@naver.com>
2022-07-12 16:40:38 +0900

    #1 Chat with WebSockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,22 +1,55 @@
+const $messageList = document.querySelector("ul");
+const $nicknameForm = document.querySelector("#nickname");
+const $messageForm = document.querySelector("#message");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-// event 종류: close / error / message / open
-// open: socket이 connection을 open 했을 때
+// open
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅");
 });
 
-// message: message를 받을 때
+// message
 socket.addEventListener("message", (message) => {
-    console.log("Just got this: ", message, " from the Server");
-    console.log(message.data);
+    console.log(message);
+
+    const $li = document.createElement("li");
+    $li.innerText = message.data;
+    $messageList.append($li);
 });
 
-// close: 서버가 오프라인이 될 때 등 socket이 close 되었을 때
+// close
 socket.addEventListener("close", () => {
     console.log("Disconnected to Server ❌");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+// 2가지 타입의 메시지가 존재 (닉네임 or 메시지)
+// 현재로서는 백엔드가 메시지 타입을 구별할 수 없음
+// 👉 text 대신 JSON 보내주기
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+function handleNickSubmit(e) {
+    e.preventDefault();
+    const $input = $nicknameForm.querySelector("input");
+    
+    // socket.send($input.value);
+    socket.send(makeMessage("nickname", $input.value));
+
+    $input.value = "";
+}
+
+function handleMsgSubmit(e) {
+    e.preventDefault();
+    const $input = $messageForm.querySelector("input");
+
+    // socket.send($input.value);
+    socket.send(makeMessage("new_message", $input.value));
+
+    $input.value = "";
+}
+
+$nicknameForm.addEventListener("submit", handleNickSubmit);
+$messageForm.addEventListener("submit", handleMsgSubmit);
--- src/server.js
+++ src/server.js
@@ -11,18 +11,42 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// fake database
+// 서버에 연결된 브라우저 목록 저장
+const sockets = [];
+
+// connection
 wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "anonymous";
     console.log("Connected to Browser ✅");
+
+    // close
     socket.on("close", () => console.log("Disconnected from Client ❌"));
+
+    // message
     socket.on("message", (message) => {
-        console.log(message.toString());
+        // 서버 - 브라우저 하나와의 통신
+        // socket.send(message.toString());
+
+        // 서버 - 브라우저 여러개와의 통신
+        // sockets.forEach(aSocket => aSocket.send(message.toString()));
+
+        // 메시지 타입 구별하기
+        const msgObj = JSON.parse(message);
+        console.log(msgObj);
+
+        if (msgObj.type === "new_message") {
+            sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${msgObj.payload}`));
+        }
+        else if (msgObj.type === "nickname") {
+            socket["nickname"] = msgObj.payload;
+        }
     })
-    socket.send("hello! client!");
 });
 
 server.listen(3000, handleListen);
