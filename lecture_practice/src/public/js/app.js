4718a76ea2dce94542e64afb36d24f8e2130454f
dianestar <dianestar@naver.com>
2022-07-20 15:47:29 +0900

    #3.2 Camera Switch

--- lecture_practice/src/public/js/app.js
+++ lecture_practice/src/public/js/app.js
@@ -1,18 +1,59 @@
 const socket = io();
 
+const $call = document.querySelector("#call");
+
+// 유저의 모든 장치 정보 얻기
+const $cameras = document.querySelector("#cameras");
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        // console.log(devices);
+
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        // console.log(cameras);
+
+        const currentCamera = myStream.getVideoTracks()[0];
+
+        cameras.forEach((camera) => {
+            const $option = document.createElement("option");
+            $option.value = camera.deviceId;
+            $option.innerText = camera.label;
+
+            if (currentCamera.label == camera.label) {
+                $option.selected = true;
+            }
+
+            $cameras.appendChild($option);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 // 비디오 & 오디오 허용
 const $myFace = document.querySelector("#myFace");
 
 let myStream;
 
-async function getMedia() {
+async function getMedia(deviceId) {
+    const initialConstraints = {
+        audio: true,
+        video: { facingMode: "user" },
+    };
+    const newConstraints = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+    };
+
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
+        myStream = await navigator.mediaDevices.getUserMedia(deviceId ? newConstraints : initialConstraints);
         // console.log(myStream);
         $myFace.srcObject = myStream;
+
+        if (!deviceId) {
+            await getCameras();
+        }
     } catch (error) {
         console.log(error);
     }
@@ -60,16 +101,11 @@ function handleCameraClick() {
 $muteBtn.addEventListener("click", handleMuteClick);
 $cameraBtn.addEventListener("click", handleCameraClick);
 
-// 유저의 장치 얻기
-async function getCameras() {
-    try {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const cameras = devices.filter((device) => device.kind === "videoinput");
-        console.log(cameras);
-    } catch (error) {
-        console.log(error);
-    }
+async function handleCameraChange() {
+    await getMedia($cameras.value);
 }
+
+$cameras.addEventListener("input", handleCameraChange);
 
 /*
 const $welcome = document.querySelector("#welcome");
